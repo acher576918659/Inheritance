918a8d91c6d50553e6384bda6476a4f6e5002dc9
acher576918659 <asd576918659@gmail.com>
2017-07-03 14:00:35 +0800

    Add files via upload

--- equals.java
+++ equals.java
@@ -11,7 +11,9 @@ class car {
 public class equals {
 
 	public static void main(String[] args) {
+		//car1:0x1234 -> 0x2345
 		car car1 = new car();
+		//car2:0x2345
 		car car2 = new car();
 		System.out.println("(car1==car2)=" + (car1 == car2));
 		System.out.println("car1.equals(car2)=" + (car1.equals(car2)));
